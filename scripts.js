c041750388b5084b7e5049af4bc9c3ebdfe7a483
Jaan Jaerving <jaj20@hi.is>
2019-11-02 20:19:24 +0000

    TODO: User edit // remove listener on delete

--- scripts.js
+++ scripts.js
@@ -1,53 +1,98 @@
 const ENTER_KEYCODE = 13;
 
 document.addEventListener('DOMContentLoaded', () => {
-  const form = document.querySelector('.form');
-  const items = document.querySelector('.items');
+    const form = document.querySelector('.form');
+    const items = document.querySelector('.items');
 
-  text.init(form, items);
+    text.init(form, items);
 });
 
 const text = (() => {
-  let items;
+    let items;
 
-  function init(_form, _items) {
-    items = _items;
-    _form.addEventListener('submit', formHandler);
+    function init(_form, _items) {
+        items = _items;
+        _form.addEventListener('submit', formHandler);
 
-    // TODO láta hluti í _items virka
-  }
+        for (let item of items.querySelectorAll('.item')) {
+            const checkbox = item.querySelector('.item__checkbox');
+            checkbox.addEventListener('click', finish);
 
-  function formHandler(e) {
-    e.preventDefault();
+            const text = item.querySelector('.item__text');
+            text.addEventListener('click', edit);
 
-    console.log('halló heimur');
-  }
+            const button = item.querySelector('.item__button');
+            button.addEventListener('click', deleteItem);
+        }
+    }
 
-  // event handler fyrir það að klára færslu
-  function finish(e) {
-  }
+    function formHandler(e) {
+        e.preventDefault();
+        const input = e.target.querySelector('.form__input');
+        const text = input.value.trim();
 
-  // event handler fyrir það að breyta færslu
-  function edit(e) {
-  }
+        if (text !== '') {
+            add(text);
+            input.value = '';
+        }
+    }
 
-  // event handler fyrir það að klára að breyta færslu
-  function commit(e) {
-  }
+    // event handler fyrir það að klára færslu
+    function finish(e) {
+        e.target.parentNode.classList.toggle('item--done');
+    }
 
-  // fall sem sér um að bæta við nýju item
-  function add(value) {
-  }
+    // event handler fyrir það að breyta færslu
+    function edit(e) {
 
-  // event handler til að eyða færslu
-  function deleteItem(e) {
-  }
+    }
 
-  // hjálparfall til að útbúa element
-  function el(type, className, clickHandler) {
-  }
+    // event handler fyrir það að klára að breyta færslu
+    function commit(e) {
 
-  return {
-    init: init
-  }
+    }
+
+    // fall sem sér um að bæta við nýju item
+    function add(value) {
+        const item = el('li', 'item');
+        const checkbox = el('input', 'item__checkbox', finish);
+        const text = el('span', 'item__text', edit);
+        const button = el('button', 'item__button', deleteItem);
+
+        checkbox.setAttribute('type', 'checkbox');
+        text.appendChild(document.createTextNode(value));
+        button.appendChild(document.createTextNode('Eyða'));
+
+        item.appendChild(checkbox);
+        item.appendChild(text);
+        item.appendChild(button);
+        items.appendChild(item);
+    }
+
+    // event handler til að eyða færslu
+    function deleteItem(e) {
+        const parent = e.target.parentNode;
+        parent.parentNode.removeChild(parent);
+
+        //TODO remove listeners ???
+    }
+
+    // hjálparfall til að útbúa element
+    function el(type, className, clickHandler) {
+        const element = document.createElement(type);
+
+        if (className) {
+            element.classList.add(className);
+        }
+
+        if (clickHandler) {
+            element.addEventListener('click', clickHandler);
+        }
+
+        return element;
+    }
+
+    return {
+        init: init
+    }
 })();
